65072b34804e9c2cce5688bceb3c4d262bb25eb7
melon <375591580@qq.com>
2019-02-09 22:46:08 +0800

    修复对vector操作不当导致的BUG

--- login_android_grpc_djinni/src/cpp/network_result.cpp
+++ login_android_grpc_djinni/src/cpp/network_result.cpp
@@ -5,6 +5,10 @@
 #include <stdio.h>
 #include <network_result.h>
 #include <sstream>
+#include <android/log.h>
+
+#define TAG    "com.wechat.mylogin"
+#define LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,TAG,__VA_ARGS__)
 
 namespace demo{
 
@@ -18,9 +22,14 @@ namespace demo{
 //         int i;
 //       ss>>i;
 //        this->code = i;
+        LOGD("aaaaaaa");
         std::string::size_type sz;   // alias of size_t
+        LOGD("bbbbbb");
         int i_dec = std::stoi (code,&sz);
+        LOGD("cccccc");
         this->code = i_dec;
+        LOGD("ddddddd");
+
     }
     std::string ReqResult::getMsg(){
         return this->msg;
