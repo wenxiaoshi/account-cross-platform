09602277eda3212e418f47a9c35b9a6be1a1db72
melon <375591580@qq.com>
2019-02-10 16:24:35 +0800

    删除无用代码 and update .gitignore

--- login_android_grpc_djinni/src/cpp/network_result.cpp
+++ login_android_grpc_djinni/src/cpp/network_result.cpp
@@ -7,29 +7,15 @@
 #include <sstream>
 #include <android/log.h>
 
-#define TAG    "com.wechat.mylogin"
-#define LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,TAG,__VA_ARGS__)
-
 namespace demo{
 
     int ReqResult::getCode(){
         return this->code;
     }
     void ReqResult::setCode(std::string code){
-//        std::string s = "17";
-//        std::stringstream ss;
-//        ss<<s;
-//         int i;
-//       ss>>i;
-//        this->code = i;
-        LOGD("aaaaaaa");
         std::string::size_type sz;   // alias of size_t
-        LOGD("bbbbbb");
         int i_dec = std::stoi (code,&sz);
-        LOGD("cccccc");
         this->code = i_dec;
-        LOGD("ddddddd");
-
     }
     std::string ReqResult::getMsg(){
         return this->msg;
