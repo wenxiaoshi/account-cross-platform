5a46c7cb859c4375423b4d9eba421646270f60e1
root <root@kind-unicorn-2.localdomain>
2019-05-26 16:26:49 +0800

    成功加入redis，并在容器中能使用

--- login_back_grpc_bazel_docker/source/cpp/account/account_server.cc
+++ login_back_grpc_bazel_docker/source/cpp/account/account_server.cc
@@ -14,6 +14,7 @@
 #include <set>
 
 #include <grpcpp/grpcpp.h>
+#include <hiredis/hiredis.h>
 
 #include "source/db/my_db.h"
 #include "utils/common_utils.h"
@@ -566,8 +567,39 @@ void RunServer() {
   server->Wait();
 }
 
+void doTest()
+{
+	//redis默认监听端口为6387 可以再配置文件中修改
+	redisContext* redis = redisConnect("144.34.177.181", 6379);
+	if ( NULL == redis || redis->err)
+	{       // redis为NULL与redis->err是两种不同的错误，若redis->err为true，可使用redis->errstr查看错误信息
+		printf("Connect to redisServer faile %s \n",redis->errstr);
+		redisFree(redis);
+		return;
+	}
+	printf("Connect to redisServer Success\n");
+	const char* command1 = "set stest1 value1";
+	redisReply* reply = (redisReply*)redisCommand(redis, command1);    // 执行命令，结果强转成redisReply*类型
+	if( NULL == reply)
+	{
+		printf("Execut command1 failure\n");
+		redisFree(redis);     // 命令执行失败，释放内存
+		return;
+	}
+	if( !(reply->type == REDIS_REPLY_STATUS && strcasecmp(reply->str,"OK")==0))
+	{       // 判断命令执行的返回值
+		printf("Failed to execute command[%s]\n",command1);
+		freeReplyObject(reply);
+		redisFree(redis);
+		return;
+	}	
+	freeReplyObject(reply);
+	printf("Succeed to execute command[%s]\n", command1);
+	// 一切正常，则对返回值进行处理
+}
+
 int main(int argc, char** argv) {
-   
+   doTest();   
    RunDb();
    RunServer();
    return 0;
